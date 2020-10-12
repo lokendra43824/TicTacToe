0d54e5fc96963c6c10c59610d0ee59b9230870d9
Lokendra <lokilokendra12345@gmail.com>
2020-10-12 15:20:16 +0530

    {lokendra] initialized first commit

---
+++ TicTacToeFile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicTacToeFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to tic tac game");
+        }
+    }
+}
